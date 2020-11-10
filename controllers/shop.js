6bbaf255fbf4e11da4d12bca20dffc1a3f3851cf
Leonardo <leosud099@gmail.com>
2020-11-09 22:42:07 -0700

    Added payment methods | checkout

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const Order = require("../models/order");
 const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
+const stripe = require('stripe')('sk_test_51HloxoAJC7coGwhj2ucxr6jFz7gYUSidzQNBqZD00pDnwKhL4cJvG8SxwPQ0oRaCRxml3oS6u0ju4vm5Or27o5rN00ADEsLhvR');
 
 const ITEMS_PER_PAGE = 2;
 
@@ -99,6 +100,45 @@ exports.postCartDeleteProduct = (req, res, next) => {
       res.redirect("/cart");
     })
     .catch((err) => console.log(err));
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+  .populate("cart.items.productId")
+  .execPopulate()
+  .then((user) => {
+    products = user.cart.items;
+    products.forEach(p => {
+      total += p.quantity * p.productId.price;
+    })
+
+    return stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: products.map(p => {
+        return {
+          name: p.productId.title,
+          description: p.productId.description,
+          amount: p.productId.price * 100,
+          currency: 'usd',
+          quantity: p.quantity
+        }
+      }),
+      success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+      cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+    });
+  })
+  .then(session => {
+    res.render("shop/checkout", {
+      path: "/checkout",
+      pageTitle: "Checkout",
+      products: products,
+      totalSum: total,
+      sessionId: session.id
+    });
+  })
+  .catch((err) => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -21,7 +21,11 @@ router.post("/cart", isAuth, shopController.postCart);
 
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
-router.post("/create-order", isAuth, shopController.postOrder);
+router.get('/checkout', isAuth, shopController.getCheckout);
+
+router.get('/checkout/success', isAuth, shopController.postOrder);
+
+router.get('/checkout/cancel',  shopController.getCheckout);
 
 router.get("/orders", isAuth, shopController.getOrders);
 
