9bb69681dc3cb84ee02e749e534cfc5222ec5a62
Leonardo <leosud099@gmail.com>
2020-11-09 21:46:10 -0700

    Async delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -175,8 +175,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -187,10 +187,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+     res.status(200).json({message: "Product deleted"});
     })
     .catch((err) => {
-      console.log("ERROR WHEN DELETING A PRODUCT \n" + err);
-      res.redirect("/500");
+      res.status(500).json({message: "Deletion failed"});
     });
 };
